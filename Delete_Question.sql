178077d6086281400fe4aa0e747fd77b68ce16b3
Satyam Kumar <satyamkumar6372@gmail.com>
2026-01-21 16:08:38 +0530

    Create Delete_Question.sql

---
+++ Delete_Question.sql
@@ -0,0 +1,22 @@
+DELETE QUESTIONS
+Delete employees whose salary is less than 35000.
+Delete employees joined in 2024.
+Delete inactive employees from Pune.
+Delete employees whose email does not contain their name.
+Delete employees having experience less than department average.
+Delete duplicate emails if any (keep lowest emp_id).
+Delete employees whose last increment is older than 3 years.
+Delete employees where name length < 4.
+Delete all HR employees with salary above 42000.
+Delete employees whose salary is below company average and inactive.
+Delete employees joined in same year as their last increment.
+Delete employees whose dept starts with 'P' and salary < 60000.
+Delete employees having experience > 7 and salary < 1 lakh.
+Delete records where city is NULL or blank.
+Delete employees whose name contains double 'a'.
+Delete all employees except top 5 highest salary.
+Delete employees who never got increment after joining.
+Delete employees where salary is not multiple of 5000.
+Delete employees where email domain is not gmail.com.
+Delete employees whose experience is greater than years since joining.
+
