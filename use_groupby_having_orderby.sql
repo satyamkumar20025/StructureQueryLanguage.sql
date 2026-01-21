7d9b8f08c6af22ee4ddf8a7125aa4b86a517f1c1
Satyam Kumar <satyamkumar6372@gmail.com>
2026-01-21 07:37:30 +0530

    Create use_groupby_having_orderby.sql

---
+++ use_groupby_having_orderby.sql
@@ -0,0 +1,17 @@
+
+Q1. Find department wise total salary for employees from Delhi or Pune. Show only departments where total salary is greater than 1,50,000 and sort result by total salary descending.
+Q2. Find city wise average salary for employees having experience more than 3 years. Display only cities where average salary is greater than 70,000 and order by average salary high to low.
+Q3. Find project wise total hours worked excluding project Testing Tool. Show only projects where total hours are greater than 200 and order by hours ascending.
+Q4. Find department wise maximum salary for employees working in Mumbai or Pune. Show only departments where maximum salary is greater than 85,000 and order by department name.
+Q5. Find city wise employee count where salary is between 60,000 and 95,000. Show only cities having more than 2 employees and order by employee count descending.
+Q6. Find project wise average rating where hours worked is greater than 90. Show only projects where average rating is greater than or equal to 4 and order by rating descending.
+Q7. Find department wise total salary for employees having experience greater than or equal to 4. Show only departments where total salary is greater than 2,00,000 and order by total salary.
+Q8. Find city wise minimum salary except city Delhi. Show only cities where minimum salary is greater than 60,000 and order by minimum salary descending.
+Q9. Find project wise total bill amount where individual bill is greater than 50,000. Show only projects where total bill is greater than 1,00,000 and order by total bill.
+Q10. Find department wise average experience for employees from Pune city. Show only departments where average experience is greater than 4 and order by experience descending.
+Q11. Find city wise count of employees having experience greater than or equal to 4 years. Show only cities where count is greater than 1 and order by count.
+Q12. Find project wise total hours worked excluding project HR Portal. Show only projects where total hours are greater than 150 and order by hours descending.
+Q13. Find project wise maximum bill amount. Show only projects where maximum bill is greater than 1,00,000 and order by maximum bill.
+Q14. Find department wise total salary only for departments Java and DevOps. Show only departments where total salary is greater than 1,60,000 and order by department.
+Q15. Find project wise average bill amount where rating is greater than or equal to 4. Show only projects where average bill is greater than 90,000 and order b  y average bill descending.
+
