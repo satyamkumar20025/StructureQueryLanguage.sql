4f56a3b6a446eb80e0c34852dc064c59d3b42972
Satyam Kumar <satyamkumar6372@gmail.com>
2026-03-03 18:33:40 +0530

    Create Subquery&Nested_Question.sql

---
+++ Subquery&Nested_Question.sql
@@ -0,0 +1,29 @@
+✍️ Section A –Basic
+
+Q1.Write an SQL query to display all students whose marks are greater than the average marks.
+
+
+Q2.Write an SQL query to find the number of students in each course.
+
+
+Q3.Write an SQL query to display employees whose salary is greater than the average salary of all employees.
+
+Q4.Write an SQL query to display courses having more than 3 students.
+
+Q5.Write an SQL query to display employees who joined after 1st January 2022.
+
+
+✍️ Section B – Medium SQL Queries
+
+Q6.Write an SQL query to display students who scored the maximum marks.
+
+Q7.Write an SQL query to display employees earning the second highest salary.
+
+Q8.Write an SQL query to display departments where the average salary is greater than 50,000.
+
+Q9.Write an SQL query to display students enrolled in the same course as 'Rahul'.
+
+Q10.Write an SQL query to display departments having more than 5 employees.
+
+
+
